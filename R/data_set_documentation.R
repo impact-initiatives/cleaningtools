711cbf80bfaf6de8b8789f674543839918d85d48
Md Mehedi Hasan KHAN <plannermehedi@gamil.com>
2023-02-09 16:36:01 +0300

    fixing cmd pass

---
+++ R/data_set_documentation.R
@@ -0,0 +1,85 @@
+
+############################### data documentation #######################
+
+
+
+
+#' Survey data
+#'
+#' These data sets include HH data (raw and clean data) and analysis along with cleaning log.
+#'
+#'
+#' @format NULL
+#' @examples
+#' analysis_by_group
+#' raw_data
+#' cleaning_log
+#' clean_data
+#' survey
+#' choices
+#' overall_analysis
+
+
+#' @name analysis_by_group
+#' @title Analysis by population group
+#' @rdname analysis_by_group
+#' @format NULL
+"analysis_by_group"
+
+#' @title Raw data
+#' @name raw_data
+#' @rdname raw_data
+#' @format NULL
+"raw_data"
+
+
+
+#' @title Cleaning log
+#' @name cleaning_log
+#' @rdname cleaning_log
+#' @format NULL
+"cleaning_log"
+
+
+
+
+#' @title Clean data
+#' @name clean_data
+#' @rdname clean_data
+#' @format NULL
+"clean_data"
+
+
+
+
+#' @title Survey tab of kobo tool
+#' @name survey
+#' @rdname survey
+#' @format NULL
+"survey"
+
+
+
+#' @title Choices tab of kobo tool
+#' @name choices
+#' @rdname choices
+#' @format NULL
+"choices"
+
+
+
+
+#' @title Nation/all population level analysis.
+#' @name overall_analysis
+#' @rdname overall_analysis
+#' @format NULL
+"overall_analysis"
+
+#' @title Dataset with food consumption, household hunger Score component
+#' @name food_consumption_df
+#' @rdname food_consumption_df
+#' @format NULL
+"food_consumption_df"
+
+
+
