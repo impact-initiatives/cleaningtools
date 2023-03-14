f1efee87a33a032ca7bb1a40dc16ee1bbfe448f6
yannsay-impact <yann.say@impact-initiatives.org>
2023-03-14 10:06:53 +0100

    correcting add_peradd_percentage_missing

--- tests/testthat/test-check_shortest_path.R
+++ tests/testthat/test-check_shortest_path.R
@@ -1,26 +1,5 @@
 #add_percentage_missing
 
-# test that inputs is correct -> dataset and kobo?
-test_that("Test that if input is not dataset, nor a list with checked_dataset it makes some errors", {
-  test_list <- list(
-    dataset = data.frame(
-      `_uuid` = c(letters[1:4], "a", "b", "c"),
-      col_a = runif(7),
-      col_b = runif(7)
-    ),
-    outlier_log = data.frame(
-      uuid = NULL,
-      value = NULL,
-      variable = NULL,
-      issue = NULL
-    )
-  )
-  testthat::expect_error(
-    add_percentage_missing(test_list),
-    "Cannot identify the dataset in the list"
-  )
-})
-
 test_that("Test that if kobo is not in a correct format it will throw an error", {
   test_data <- data.frame(
     uuid = c(letters[1:4], "a", "b", "c"),
@@ -63,16 +42,6 @@ test_that("test that if the new names exists, it will stop", {
 
 })
 
-test_that("test that if uuid column does not exist, it will stop", {
-  test_data <- data.frame(uuid = c(1:3))
-  expect_error(add_percentage_missing(test_data),
-               "Cannot find _uuid in the names of the dataset")
-
-  test_data2 <- data.frame(id = c(1:3))
-  expect_error(add_percentage_missing(test_data, uuid_col = "X_uuid"),
-               "Cannot find X_uuid in the names of the dataset")
-})
-
 test_that("test that the columns are added with the correct values with a kobo", {
   data_test <- data.frame(uuid = c(1:3),
                           col_1 = c(1:3),
@@ -95,7 +64,7 @@ test_that("test that the columns are added with the correct values with a kobo",
                                  col_4.health = c(1,NA,0),
                                  col_4.school = c(1,NA,0),
                                  percentage_missing = c(.25, .75,0))
-  expect_equal(add_percentage_missing(data_test, kobo_survey = kobo_survey, uuid_col = "uuid"),
+  expect_equal(add_percentage_missing(data_test, kobo_survey = kobo_survey),
                expected_results)
 })
 test_that("test that the columns are added with the correct values without a kobo", {
@@ -118,7 +87,7 @@ test_that("test that the columns are added with the correct values without a kob
                                  col_4.health = c(1,NA,0),
                                  col_4.school = c(1,NA,0),
                                  percentage_missing = c(1/8, 6/8,0))
-  expect_equal(add_percentage_missing(data_test, uuid_col = "uuid"),
+  expect_equal(add_percentage_missing(data_test),
                expected_results)
 })
 test_that("it returns the dataframe with only 1 added column", {
