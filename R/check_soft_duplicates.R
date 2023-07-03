a065041137bbca5f1bd59aa73eaa38e02f358614
AbrahamAz <abraham.azar30@outlook.com>
2023-07-03 14:45:02 +0200

    minor edits fixes

--- R/check_soft_duplicates.R
+++ R/check_soft_duplicates.R
@@ -1,52 +1,70 @@
 #' Checks for survey similarities - Soft Duplicates
 #'
-#' @param dataset RawData to be checked as a dataframe or a list with dataframe
+#' @param .dataset RawData to be checked as a dataframe or a list with dataframe
 #'  stored as "checked_dataset"
 #' @param kobo_survey Kobo Survey Sheet
 #' @param uuid String character for the uuid column. By default "uuid"
 #' @param idnk_value String character for the value of the "I don't know" value
 #' @param sm_seperator Separator for choice multiple questions. The default is "."
 #' @param log_name Name of the log dataframe flagged in the end of the function
+#' @param threshold flag all entries below specified threshold. The default is 7.
 #'
 #' @return return a list with the dataset checked stored as checked_dataset and
 #' a dataframe with the soft duplicate log
 #' @export
 #'
 #' @examples
-#' soft_duplicates <- check_soft_duplicates(dataset = cleaningtools_raw_data,
+#' soft_duplicates <- check_soft_duplicates(.dataset = cleaningtools_raw_data,
 #'                                          kobo_survey = cleaningtools_survey,
 #'                                          uuid = "X_uuid",
 #'                                          idnk_value = "dont_know",
 #'                                          sm_seperator = ".",
-#'                                          log_name = "soft_duplicate_log")
+#'                                          log_name = "soft_duplicate_log",
+#'                                          threshold = 7)
+#' group_by_enum_raw_data <- cleaningtools_raw_data %>%
+#'   dplyr::group_by(enumerator_num)
+#'
+#' soft_per_enum <- group_by_enum_raw_data %>%
+#'   dplyr::group_split() %>%
+#'   purrr::map(~check_soft_duplicates(.dataset = .,
+#'                                     kobo_survey = cleaningtools_survey,
+#'                                     uuid = "X_uuid",idnk_value = "dont_know",
+#'                                     sm_seperator = ".",
+#'                                     log_name = "soft_duplicate_log",
+#'                                     threshold = 7))
+#' soft_per_enum %>%
+#'   purrr::map(~.[["soft_duplicate_log"]]) %>%
+#'   purrr::map2(.y = dplyr::group_keys(group_by_enum_raw_data) %>% unlist(),
+#'               ~dplyr::mutate(.x, enum = .y)) %>%
+#'   do.call(dplyr::bind_rows,.)
 
-check_soft_duplicates <- function(dataset,
+check_soft_duplicates <- function(.dataset,
                                   kobo_survey,
                                   uuid="uuid",
                                   idnk_value="idnk",
                                   sm_seperator = ".",
-                                  log_name = "soft_duplicate_log"){
-
-  if(is.data.frame(dataset)){
-    dataset <- list(checked_dataset = dataset)
+                                  log_name = "soft_duplicate_log",
+                                  threshold = 7){
+  if(is.data.frame(.dataset)){
+    .dataset <- list(checked_dataset = .dataset)
   }
 
-  if(!"checked_dataset" %in% names(dataset)) {
+  if(!"checked_dataset" %in% names(.dataset)) {
     stop("Cannot identify the dataset in the list.")
   }
 
-  if(!uuid %in% names(dataset[["checked_dataset"]])){
+  if(!uuid %in% names(.dataset[["checked_dataset"]])){
     msg <- glue::glue("Cannot find ", uuid, " in the names of the dataset")
     stop(msg)
   }
 
-  data <- dataset$checked_dataset
+  df <- .dataset$checked_dataset
   # 1) store UUIDs
-  uuids <- data[[uuid]]
+  uuids <- df[[uuid]]
 
   # 2) convert all columns to character and tolower
-  data <- dplyr::mutate_all(data, as.character)
-  data <- dplyr::mutate_all(data, tolower)
+  df <- dplyr::mutate_all(df, as.character)
+  df <- dplyr::mutate_all(df, tolower)
 
   # 3) remove columns that are naturally different in each survey:
   # - columns of type = "start", "end", etc.
@@ -55,46 +73,44 @@ check_soft_duplicates <- function(dataset,
   # - columns starting with special characters
   types_to_remove <- c("start", "end", "today", "deviceid", "date", "geopoint", "audit",
                        "note", "calculate","begin_group","end_group")
-  sm_parents<-auto_detect_sm_parents(data, sm_seperator)
-  sm_columns <- data %>%
+  sm_parents<- auto_detect_sm_parents(df, sm_seperator)
+  sm_columns <- df %>%
     dplyr::select(dplyr::starts_with(glue::glue("{sm_parents}{sm_seperator}"))) %>%
     colnames()
-  cols_to_keep <- data.frame(column=colnames(data)) %>%
+  cols_to_keep <- data.frame(column=colnames(df)) %>%
     dplyr::left_join(select(kobo_survey, name, type), by=c("column"="name"))%>%
+    dplyr::filter(!is.na(type)) %>%
     dplyr::filter((!(type %in% types_to_remove) & !stringr::str_starts(column, "X_") &
                      !(stringr::str_starts(column, "[[:punct:]]")) & !(column %in% sm_columns)))
-  data <- data[, cols_to_keep$column]
+  df <- df[, cols_to_keep$column]
 
   # 4) remove columns with all NA; convert remaining NA to "NA"; convert all columns to factor
-  data <- data[, colSums(is.na(data))<nrow(data)]
-  data[is.na(data)] <- "NA"
-  data <- data %>% dplyr::mutate_if(is.character, factor)
+  df <- df[, colSums(is.na(df))<nrow(df)]
+  df[is.na(df)] <- "NA"
+  df <- df %>% dplyr::mutate_if(is.character, factor)
   error.message <- "NAs detected, remove them before proceeding (it can happen when converting to factor)"
-  if (sum(is.na(data))>0) stop(error.message)
+  if (sum(is.na(df))>0) stop(error.message)
 
   # 5) calculate gower distance
-  gower_dist <- cluster::daisy(data, metric="gower", warnBin=F, warnAsym=F, warnConst=F)
+  gower_dist <- cluster::daisy(df, metric="gower", warnBin=F, warnAsym=F, warnConst=F)
   gower_mat <- as.matrix(gower_dist)
 
   # 6) convert distance to number of differences and determine closest matching survey
-  r <- unlist(lapply(1:nrow(data), function(i){
-    srv1 <- sort(gower_mat[i,]*ncol(data))[1]
-    srv2 <- sort(gower_mat[i,]*ncol(data))[2]
-    if (names(srv1)==as.character(i)) return(srv2)
-    else return(srv1)
+  r <- unlist(lapply(1:nrow(df), function(i){
+    srv1 <- sort(gower_mat[i,]*ncol(df))[2]
+    return(srv1)
   }))
 
   # 7) add relevant columns
-  outdata <- dataset$checked_dataset[,uuid]
-  outdata[["num_cols_not_NA"]] <- rowSums(data!="NA")
-  outdata[[paste0("num_cols_", idnk_value)]] <- rowSums(data==idnk_value)
-  outdata[[uuid]] <- uuids
+  outdata <- .dataset$checked_dataset[,uuid]
+  outdata[["num_cols_not_NA"]] <- rowSums(df!="NA")
+  outdata[["total_columns_compared"]] <- ncol(df)
+  outdata[[paste0("num_cols_", idnk_value)]] <- rowSums(df==idnk_value)
   outdata[["id_most_similar_survey"]] <- uuids[as.numeric(names(r))]
   outdata[["number_different_columns"]] <- as.numeric(r)
+  outdata[["issue"]][outdata[["number_different_columns"]] <= threshold] <- glue::glue("Less than ",threshold," differents options")
   outdata <- outdata %>% dplyr::arrange(number_different_columns, !!rlang::sym(uuid))
-  dataset[[log_name]] <- as.data.frame(outdata)
-
-  return(dataset)
+  .dataset[[log_name]] <- as.data.frame(outdata)
+  return(.dataset)
 }
-
 
--- tests/testthat/test-check_soft_duplicates.R
+++ tests/testthat/test-check_soft_duplicates.R
@@ -1,18 +1,18 @@
 testthat::test_that("Error checks", {
-  testthat::expect_error(check_soft_duplicates(dataset = cleaningtools::cleaningtools_sample_frame,
+  testthat::expect_error(check_soft_duplicates(.dataset = cleaningtools::cleaningtools_sample_frame,
                                                kobo_survey = cleaningtools::cleaningtools_survey,
                                                uuid = "X_uuid"))
-  testthat::expect_error(check_soft_duplicates(dataset = cleaningtools::cleaningtools_raw_data,
+  testthat::expect_error(check_soft_duplicates(.dataset = cleaningtools::cleaningtools_raw_data,
                                                kobo_survey = cleaningtools::cleaningtools_survey,
                                                uuid = "uuid"))
   t
-  testthat::expect_no_error(check_soft_duplicates(dataset = cleaningtools::cleaningtools_raw_data,
+  testthat::expect_no_error(check_soft_duplicates(.dataset = cleaningtools::cleaningtools_raw_data,
                                                   kobo_survey = cleaningtools::cleaningtools_survey,
                                                   uuid = "X_uuid",idnk_value = "dont_know"))
 })
 
 testthat::test_that("Exact equals", {
-  actual_output <- check_soft_duplicates(dataset = cleaningtools::cleaningtools_raw_data,
+  actual_output <- check_soft_duplicates(.dataset = cleaningtools::cleaningtools_raw_data,
                                          kobo_survey = cleaningtools::cleaningtools_survey,
                                          uuid = "X_uuid",idnk_value = "dont_know")[[2]] %>% head()
 
@@ -22,7 +22,8 @@ testthat::test_that("Exact equals", {
                                          "dc7bf25b-e18b-4b9e-bb34-5d7a1e762eb2",
                                          "0858486a-1d3d-492b-863f-b050cb9fe7af",
                                          "193d5f36-93b9-4c97-9205-13aa7e3a6c7f"),
-                                num_cols_not_NA = c(76,86,86,76,75,76),
+                                num_cols_not_NA = c(75,85,85,75,74,75),
+                                total_columns_compared = c(148,148,148,148,148,148),
                                 num_cols_dont_know = c(0,0,0,0,0,0),
                                 id_most_similar_survey = c("dc7bf25b-e18b-4b9e-bb34-5d7a1e762eb2",
                                                            "db5e05db-94e9-44aa-9206-3e1c17a7a233",
@@ -30,7 +31,8 @@ testthat::test_that("Exact equals", {
                                                            "3370f726-395a-4675-94fe-9e745e0b36e9",
                                                            "93893e39-9c82-4e19-b480-4dc78033157b",
                                                            "ac42b381-4d3b-42b4-96fb-676d43a8c4e7"),
-                                number_different_columns = c(10,10,10,10,11,11))
+                                number_different_columns = c(9,9,9,9,10,10),
+                                issue = c(NA_character_,NA_character_,NA_character_,NA_character_,NA_character_,NA_character_))
 
   testthat::expect_equal(actual_output,expected_output)
 
@@ -38,7 +40,7 @@ testthat::test_that("Exact equals", {
   })
 
 testthat::test_that("Check all uuids", {
-  actual_output <- check_soft_duplicates(dataset = cleaningtools::cleaningtools_raw_data,
+  actual_output <- check_soft_duplicates(.dataset = cleaningtools::cleaningtools_raw_data,
                                          kobo_survey = cleaningtools::cleaningtools_survey,
                                          uuid = "X_uuid", idnk_value = "dont_know")[[2]] %>%
     arrange(X_uuid) %>% select(X_uuid) %>% pull
@@ -47,7 +49,7 @@ testthat::test_that("Check all uuids", {
 })
 
 testthat::test_that("Returns a list of 2 dataframe", {
-  actual_output <- check_soft_duplicates(dataset = cleaningtools::cleaningtools_raw_data,
+  actual_output <- check_soft_duplicates(.dataset = cleaningtools::cleaningtools_raw_data,
                                          kobo_survey = cleaningtools::cleaningtools_survey,
                                          uuid = "X_uuid",idnk_value = "dont_know")
   testthat::expect_equal(length(actual_output),2)
